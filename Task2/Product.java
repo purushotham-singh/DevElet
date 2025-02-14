55f3ff237c7f72031a5f3a78cc0b71427c9f4798
purushotham-singh <115650874+purushotham-singh@users.noreply.github.com>
2025-02-14 23:03:46 +0530

    Task 2

---
+++ Task2/Product.java
@@ -0,0 +1,18 @@
+package Task2;
+
+class Product {
+    String name;
+    double price;
+    int stock;
+
+    public Product(String name, double price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | Price: $" + price + " | Stock: " + stock;
+    }
+}
